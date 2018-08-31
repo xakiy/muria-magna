2d495dd66504fea7c4bfa3a25ca1fd78b2c5e47a
xakiy <ghulam.zakiy@gmail.com>
2018-09-01 06:27:21 +0800

    - Add route file for easier route maintenance - test.ini as config template with more database choices to pick from

---
+++ route.py
@@ -0,0 +1,79 @@
+# Copyright 2017 Ahmad Ghulam Zakiy <ghulam (dot) zakiy (at) gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Route of resources."""
+
+from muria import auth
+from muria import personal
+#from muria import lembaga
+from muria import asrama
+#from muria import stats
+#from muria import downstreamer
+# from muria import devel
+
+static_route = []
+resource_route = []
+
+# Fitur baru di falcon 1.4.0
+static_route.append(('/web', '/home/zakiy/public_html/giriDashboard/'))
+# static_route.append('/pub', os.path.dirname(os.path.abspath(__file__)) +
+#                     '/' + config.path('storage_path'))
+static_route.append(('/ref', '/home/zakiy/public_html/sufee/'))
+
+""" Path otentikasi pengguna """
+resource_route.append(('/auth', auth.Authentication()))
+resource_route.append(('/verify', auth.Verification()))
+resource_route.append(('/refresh', auth.Refresh()))
+
+# semua personal/warga, termasuk para santri, bahkan wali santri di rumah
+resource_route.append(('/persons', personal.ResOrangs()))
+# person by uuid
+resource_route.append(('/persons/{id:uuid}', personal.ResDataOrang()))
+
+# listing data semua santri, dengan paginasi
+resource_route.append(('/santri', personal.ResSantri()))
+# data santri by uuid
+resource_route.append(('/santri/{id:uuid}', personal.ResDataSantri()))
+'''
+# listing data semua lembaga
+resource_route.append(('/lembaga', lembaga.ResLembaga()))
+# data sebuah lembaga berdasarkan slug(nick pendek)
+resource_route.append(('/lembaga/{lid}', lembaga.ResDataLembaga()))
+# listing semua jabatan berdasarkan lembaga
+resource_route.append(('/lembaga/{lid}/jabatan', lembaga.ResJabatanLembaga()))
+# listing semua jabatan berdasarkan lembaga
+resource_route.append(('/lembaga/{lid}/jabatan/{id}', lembaga.ResJabatanLembagaDetail()))
+# listing semua karyawan berdasarkan lembaga
+# resource_route.append(('/lembaga/{lid}/karyawan', lembaga.ResDataLembaga()))
+# listing semua pengurus lembaga yang berstatus santri
+# resource_route.append(('/lembaga/karyawan/santri', lembaga.sebuahLembaga()))
+# listing semua pegawai lembaga yang non-santri
+# resource_route.append(('/lembaga/karyawan/nonsantri', lembaga.sebuahLembaga()))
+'''
+resource_route.append(('/rayon', asrama.ResRayon()))
+'''
+resource_route.append(('/wilayah/kepala', asrama.ResKepalaWilayah()))
+resource_route.append(('/wilayah/{wid:int}', asrama.ResDataWilayah()))
+resource_route.append(('/wilayah/{wid:int}/blok', asrama.ResBlok()))
+
+# data statistik dalam angka
+resource_route.append(('/stats/santri', stats.ResStatsSantri()))
+resource_route.append(('/stats/santri/{jinshi}', stats.ResStatsSantriByJinshi()))
+# resource_route.append(('/stats/pengurus/putra'))
+# resource_route.append(('/stats/pengurus/putri'))
+# resource_route.append(('/stats/pengurus/wilayah'))
+# resource_route.append(('/stats/pengurus/wilayah/putra'))
+# resource_route.append(('/stats/pengurus/wilayah/'))
+
+resource_route.append(('/upload', downstreamer.DownStream()))
+'''
--- wsgi.py
+++ wsgi.py
@@ -33,56 +33,10 @@ app = application = falcon.API(middleware=middleware_list)
 
 app.req_options.auto_parse_form_urlencoded = True
 
-# Fitur baru di falcon 1.4.0
-app.add_static_route('/web', '/home/zakiy/public_html/giriDashboard/')
-# app.add_static_route('/pub', os.path.dirname(os.path.abspath(__file__)) +
-#                     '/' + config.path('storage_path'))
-app.add_static_route('/ref', '/home/zakiy/public_html/sufee/')
+from route import static_route, resource_route
 
-""" Path otentikasi pengguna """
-app.add_route('/auth', auth.Authentication())
-app.add_route('/verify', auth.Verification())
-app.add_route('/refresh', auth.Refresh())
+for (path, url) in static_route:
+    app.add_static_route(path, url)
 
-# semua personal/warga, termasuk para santri, bahkan wali santri di rumah
-app.add_route('/persons', personal.ResOrangs())
-# person by uuid
-app.add_route('/persons/{id:uuid}', personal.ResDataOrang())
-
-# listing data semua santri, dengan paginasi
-app.add_route('/santri', personal.ResSantri())
-# data santri by uuid
-app.add_route('/santri/{id:uuid}', personal.ResDataSantri())
-'''
-# listing data semua lembaga
-app.add_route('/lembaga', lembaga.ResLembaga())
-# data sebuah lembaga berdasarkan slug(nick pendek)
-app.add_route('/lembaga/{lid}', lembaga.ResDataLembaga())
-# listing semua jabatan berdasarkan lembaga
-app.add_route('/lembaga/{lid}/jabatan', lembaga.ResJabatanLembaga())
-# listing semua jabatan berdasarkan lembaga
-app.add_route('/lembaga/{lid}/jabatan/{id}', lembaga.ResJabatanLembagaDetail())
-# listing semua karyawan berdasarkan lembaga
-# app.add_route('/lembaga/{lid}/karyawan', lembaga.ResDataLembaga())
-# listing semua pengurus lembaga yang berstatus santri
-# app.add_route('/lembaga/karyawan/santri', lembaga.sebuahLembaga())
-# listing semua pegawai lembaga yang non-santri
-# app.add_route('/lembaga/karyawan/nonsantri', lembaga.sebuahLembaga())
-'''
-app.add_route('/rayon', asrama.ResRayon())
-'''
-app.add_route('/wilayah/kepala', asrama.ResKepalaWilayah())
-app.add_route('/wilayah/{wid:int}', asrama.ResDataWilayah())
-app.add_route('/wilayah/{wid:int}/blok', asrama.ResBlok())
-
-# data statistik dalam angka
-app.add_route('/stats/santri', stats.ResStatsSantri())
-app.add_route('/stats/santri/{jinshi}', stats.ResStatsSantriByJinshi())
-# app.add_route('/stats/pengurus/putra')
-# app.add_route('/stats/pengurus/putri')
-# app.add_route('/stats/pengurus/wilayah')
-# app.add_route('/stats/pengurus/wilayah/putra')
-# app.add_route('/stats/pengurus/wilayah/')
-
-app.add_route('/upload', downstreamer.DownStream())
-'''
+for (path, resource) in resource_route:
+    app.add_route(path, resource)
