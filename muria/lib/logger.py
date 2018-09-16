8d5978252c4891623302dd3b644524caafff82ae
xakiy <ghulam.zakiy@gmail.com>
2018-09-17 00:29:00 +0800

    The forgotten muria.lib.logger at last added to the repo!

---
+++ muria/lib/logger.py
@@ -0,0 +1,33 @@
+# Copyright 2017 Ahmad Ghulam Zakiy <ghulam (dot) zakiy (at) gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+Handle Muria's logging session
+"""
+
+import logging
+
+class Logger(object):
+
+    def __init__(self, config):
+        self.config = config
+
+    def get_logger(self, name):
+        logger = logging.getLogger(name)
+        logger.setLevel(self.config.getint('app', 'log_level'))
+        logger.propogate = False
+        if not logger.handlers:
+            handler = logging.StreamHandler()
+            logger.addHandler(handler)
+        return logger
