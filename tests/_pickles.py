6d7b33ddce5d84fa2accc0bb8c131ca41b12e7cc
Ahmad Ghulam Zakiy <ghulam.zakiy@gmail.com>
2019-01-22 01:49:13 +0800

    Test - _pickles:  - make sure that pickle folder is exist in    CI/CD environment before using it

--- tests/_pickles.py
+++ tests/_pickles.py
@@ -2,9 +2,11 @@
 
 import pickle
 import os
+import pathlib
+
 
 dir = 'tests/.cache'
-
+pathlib.Path(dir).mkdir(parents=True, exist_ok=True)
 
 def _pickling(stuff, filename):
     with open(os.path.join(dir, filename), 'wb') as f:
